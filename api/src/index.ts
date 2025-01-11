ac8d7ff7e5d6649cdffb548e7c57bf46e725198d
Dmitrii Lytov <lytovdmitriidev@gmail.com>
2025-01-11 21:00:57 +0200

    Added Express

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,26 @@
-let s: string
+import express from 'express'
+import OpenAI from "openai";
+import {configDotenv} from "dotenv";
+configDotenv()
 
-s = 'hi'
+const app = express()
+const port = 80
 
-console.log(s)
+console.log(process.env.GPT_API_KEY)
+
+app.get('/', async (req, res) => {
+    const openai = new OpenAI({apiKey: process.env.GPT_API_KEY});
+    const completion = await openai.chat.completions.create({
+        model: "gpt-4o",
+        store: true,
+        messages: [
+            {"role": "user", "content": "Hi!"}
+        ]
+    });
+
+    res.send(completion.choices[0].message.content)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
